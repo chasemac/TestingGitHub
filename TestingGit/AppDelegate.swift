26de47374a6a1bc32092c426087dc48d13d92e34
Chase McElroy <ichasem@gmail.com>
2021-05-28 17:11:46 -0500

    Update AppDelegate.swift

--- TestingGit/AppDelegate.swift
+++ TestingGit/AppDelegate.swift
@@ -11,7 +11,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var addedVarAfterDeletingAFileOnSecondary = true
-
+    var addedMoreStuffToSecondaryAfter = true
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
